ec428b438fcdeb5a154387fc19835019ab3d2428
S. Sauermann <SaschaSauermann@gmx.de>
2016-01-11 23:39:37 +0100

    added jscs with jquery preset + formatted source accordingly

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,84 +1,92 @@
-'use strict';
+"use strict";
 
-module.exports = function(grunt) {
+module.exports = function( grunt ) {
 
   // Project configuration.
-  grunt.initConfig({
+  grunt.initConfig( {
+
     // Metadata.
-    pkg: grunt.file.readJSON('data-events.jquery.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+    pkg: grunt.file.readJSON( "data-events.jquery.json" ),
+    banner: "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+
     // Task configuration.
     clean: {
-      files: ['dist']
+      files: [ "dist" ]
     },
     concat: {
       options: {
-        banner: '<%= banner %>',
+        banner: "<%= banner %>",
         stripBanners: true
       },
       dist: {
-        src: ['src/jquery.<%= pkg.name %>.js'],
-        dest: 'dist/jquery.<%= pkg.name %>.js'
-      },
+        src: [ "src/jquery.<%= pkg.name %>.js" ],
+        dest: "dist/jquery.<%= pkg.name %>.js"
+      }
     },
     uglify: {
       options: {
-        banner: '<%= banner %>'
+        banner: "<%= banner %>"
       },
       dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/jquery.<%= pkg.name %>.min.js'
-      },
+        src: "<%= concat.dist.dest %>",
+        dest: "dist/jquery.<%= pkg.name %>.min.js"
+      }
     },
     qunit: {
-      files: ['test/**/*.html']
+      files: [ "test/**/*.html" ]
     },
     jshint: {
       options: {
         jshintrc: true
       },
       gruntfile: {
-        src: 'Gruntfile.js'
+        src: "Gruntfile.js"
       },
       src: {
-        src: ['src/**/*.js']
+        src: [ "src/**/*.js" ]
       },
       test: {
-        src: ['test/**/*.js']
-      },
+        src: [ "test/**/*.js" ]
+      }
     },
     watch: {
       gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
+        files: "<%= jshint.gruntfile.src %>",
+        tasks: [ "jshint:gruntfile" ]
       },
       src: {
-        files: '<%= jshint.src.src %>',
-        tasks: ['jshint:src', 'qunit']
+        files: "<%= jshint.src.src %>",
+        tasks: [ "jshint:src", "qunit" ]
       },
       test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'qunit']
-      },
+        files: "<%= jshint.test.src %>",
+        tasks: [ "jshint:test", "qunit" ]
+      }
     },
-  });
+    jscs: {
+        gruntfile: [ "Gruntfile.js" ],
+        src: [ "src/**/*.js" ],
+        test: [ "test/**/*.js" ]
+    }
+  } );
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks( "grunt-contrib-clean" );
+  grunt.loadNpmTasks( "grunt-contrib-concat" );
+  grunt.loadNpmTasks( "grunt-contrib-uglify" );
+  grunt.loadNpmTasks( "grunt-contrib-qunit" );
+  grunt.loadNpmTasks( "grunt-contrib-jshint" );
+  grunt.loadNpmTasks( "grunt-contrib-watch" );
+  grunt.loadNpmTasks( "grunt-jscs" );
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'clean', 'concat', 'uglify']);
-    
+  grunt.registerTask( "default", [ "jshint", "jscs", "qunit", "clean", "concat", "uglify" ] );
+
   //Travis CI task
-  grunt.registerTask('travis', ['jshint', 'qunit'])
+  grunt.registerTask( "travis", [ "jshint", "jscs", "qunit" ] );
 
 };
--- src/data-events.js
+++ src/data-events.js
@@ -1,219 +1,227 @@
-/*jslint browser: true, es5: true, evil: true */
-/*global $, jQuery, alert, console*/
-(function ($) {
+( function( $ ) {
     "use strict";
     var debug = false;
-    
+
     /*
     Returns reference id of element
     data-id@='abc12' -> 'abc12'
     */
-    function getDataId(e) {
-        return e.data('id@');
+    function getDataId( e ) {
+        return e.data( "id@" );
     }
-    
+
     /*
-     Returns a object of all data attributes beginning with an @ and their values of a given dom element
+     Returns a object of all data attributes beginning with an @ and
+     their values of a given dom element
      {@name: value1, @other: value2}
      */
-    function getAttributes(e) {
-        /* 
-         returns all HTML5 custom data attributes as an object:
-         <div id='prod' data-id='10' data-cat='toy' data-cid='42'>blah</div> -> { "id":10, "cat":"toy", "cid":42 }
+    function getAttributes( e ) {
+        /*
+         Returns all HTML5 custom data attributes as an object:
+         <div id='prod' data-id='10' data-cat='toy' data-cid='42'>blah</div>
+         -> { "id":10, "cat":"toy", "cid":42 }
          jQuery (>=1.4.4)
          */
         var dataDict = e.data();
-        
+
         //Filter data attributes for @ character
-        $.each(dataDict, function (key, value) {
-            if (key.indexOf('@') !== 0) {
-                delete dataDict[key];
+        $.each( dataDict, function( key ) {
+            if ( key.indexOf( "@" ) !== 0 ) {
+                delete dataDict[ key ];
             }
-        });
-        
+        } );
+
         return dataDict;
-        
+
     }
-    
+
     /*
     Returns an array of all @-data attribute objects of each child
     [[dom1, {@name: value1}], [dom2, {@name: value2, @other: value3}]]
     */
-    function getChildAttributes(e) {
-        
+    function getChildAttributes( e ) {
+
         var arry = [];
-        
-        e.find('*').each(function () {
-            arry.push([$(this), getAttributes($(this))]);
-        });
-        
+
+        e.find( "*" ).each( function() {
+            arry.push( [ $( this ), getAttributes( $( this ) ) ] );
+        } );
+
         return arry;
-        
+
     }
-    
+
     /*
-    resolve cross references and validates structure
+    Resolve cross references and validates structure
     */
-    function resolveReference(childAttr) {
-                
+    function resolveReference( childAttr ) {
+
         //[[dom1, {@name: value1}], [dom2, {@name: value2, @other: value3}]]
-        $.each(childAttr, function (i, arry) {
-            if (arry.length < 2) {
+        $.each( childAttr, function( i, arry ) {
+            if ( arry.length < 2 ) {
                 throw "Missing data: [dom, attribute]";
             }
-            var dom = arry[0],
-                attr = arry[1];
-            
+
+            //Var dom = arry[0],
+              var attr = arry[ 1 ];
+
             //{@name: value2, @other: value3}
-            $.each(attr, function (k, v) {
-                
-                if (typeof (v) !== 'object') {
-                    console.error(attr);
-                    throw "Format has to be: data-@name={event, handler} or {id, attribute} for a reference'";
+            $.each( attr, function( k, v ) {
+
+                if ( typeof ( v ) !== "object" ) {
+                    console.error( attr );
+                    throw "Format has to be: data-@name={event, handler}" +
+                        "or {id, attribute} for a reference'";
                 }
 
-                var cyclicTest = [v],
+                var cyclicTest = [ v ],
 
                 //Search for reference until finding a non reference
-                    searchForReference = function (ref) {
-                        
-                        if (debug) {
-                            console.log("Resolving reference for: ");
-                            console.log(ref);
+                    searchForReference = function( ref ) {
+
+                        if ( debug ) {
+                            console.log( "Resolving reference for: " );
+                            console.log( ref );
                         }
 
                         //Filter valid references
-                        var validIds = $.grep(childAttr, function (arry) {
+                        var validIds = $.grep( childAttr, function( arry ) {
 
-                                var id = getDataId(arry[0]);
-                                return (id === ref.id);
+                                var id = getDataId( arry[ 0 ] );
+                                return ( id === ref.id );
 
-                            }),
+                            } ),
                             refAttribute;
 
-                        if (validIds.length === 0) {
+                        if ( validIds.length === 0 ) {
                             throw "Reference id not found: " + ref.id;
                         }
-                        if (validIds.length > 1) {
+                        if ( validIds.length > 1 ) {
                             throw "Reference id is ambigous: " + ref.id;
                         }
 
                         //Get referenced attribute
-                        refAttribute = validIds[0][0].data(ref.attribute);
+                        refAttribute = validIds[ 0 ][ 0 ].data( ref.attribute );
 
                         //Found another reference or am I done?
-                        if (typeof (refAttribute.event) !== 'undefined') {
-                            if (debug) {
-                                console.log("Resolved to: ");
-                                console.log(refAttribute);
+                        if ( typeof ( refAttribute.event ) !== "undefined" ) {
+                            if ( debug ) {
+                                console.log( "Resolved to: " );
+                                console.log( refAttribute );
                             }
                             return refAttribute;
                         }
+
                         //--> another reference
-                        if (debug) {
-                            console.log(refAttribute);
+                        if ( debug ) {
+                            console.log( refAttribute );
                         }
+
                         //Test for cyclic dependencies
-                        if ($.inArray(refAttribute, cyclicTest)) {
+                        if ( $.inArray( refAttribute, cyclicTest ) ) {
                             throw "References are cyclic";
                         }
 
-                        //continue search
-                        searchForReference(refAttribute);
+                        //Continue search
+                        searchForReference( refAttribute );
                     };
 
                 //If attribute is a reference
-                if (typeof (v.event) === 'undefined') {
+                if ( typeof ( v.event ) === "undefined" ) {
+
                     //Replace reference with resolved value
-                    attr[k] = searchForReference(v);
+                    attr[ k ] = searchForReference( v );
                 }
-            
-            });
-        });
-        
+
+            } );
+        } );
+
         return childAttr;
     }
-    
+
     /*
-    handle an event and update the attribute with the calculated value
+    Handle an event and update the attribute with the calculated value
     */
-    function handleEvent(dom, attributename, event, handler, value) {
-        if (debug) {
-            console.log('Handling event:');
-            console.log(dom);
-            console.log(attributename);
-            console.log(handler);
-            console.log(value);
+    function handleEvent( dom, attributename, event, handler, value ) {
+        if ( debug ) {
+            console.log( "Handling event:" );
+            console.log( dom );
+            console.log( attributename );
+            console.log( handler );
+            console.log( value );
         }
-        
+
         var resultValue;
-        
-        if (typeof (handler) === 'object') {
-            resultValue = handler[value];
-        } else if (typeof (handler) === 'undefined') {
+
+        if ( typeof ( handler ) === "object" ) {
+            resultValue = handler[ value ];
+        } else if ( typeof ( handler ) === "undefined" ) {
             resultValue = value;
         } else {
-            resultValue = eval('(' + handler + ')')(event, value);
+            resultValue = eval( "(" + handler + ")" )( event, value );
         }
-        
-        if (typeof (resultValue) === 'undefined') {
+
+        if ( typeof ( resultValue ) === "undefined" ) {
             throw "Handler is invalid: " + handler;
         }
-        
-        //camel case format of jquery to dashed format helloWorld -> hello-world
-        dom.attr(attributename.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(), resultValue);
+
+        //Camel case format of jquery to dashed format helloWorld -> hello-world
+        dom.attr( attributename.replace( /([a-z])([A-Z])/g, "$1-$2" ).toLowerCase(), resultValue );
     }
-    
+
     /*
     Execute an event with the given value on an dom element
     */
-    function executeEvent(at, event, eventValue) {
-        if (debug) {
-            console.log('Executing event:');
-            console.log(event);
-            console.log(eventValue);
+    function executeEvent( at, event, eventValue ) {
+        if ( debug ) {
+            console.log( "Executing event:" );
+            console.log( event );
+            console.log( eventValue );
         }
-        
-        //filter events from each object
-        $.each(at, function (i, domAt) {
-            $.each(domAt[1], function (key, value) {
-                 //multiple events given as array
-                if (typeof (value.event) === 'object') {
-                    $.each(value.event, function (i, val) {
-                        if (val === event) {
-                            handleEvent(domAt[0], key.substring(1), value.event, value.handler, eventValue);
+
+        //Filter events from each object
+        $.each( at, function( i, domAt ) {
+            $.each( domAt[ 1 ], function( key, value ) {
+
+                 //Multiple events given as array
+                if ( typeof ( value.event ) === "object" ) {
+                    $.each( value.event, function( i, val ) {
+                        if ( val === event ) {
+                            handleEvent( domAt[ 0 ], key.substring( 1 ),
+                                        value.event, value.handler, eventValue );
                         }
-                    });
+                    } );
                 } else {
-                    //single event given as string
-                    if (value.event === event) {
-                        handleEvent(domAt[0], key.substring(1), value.event, value.handler, eventValue);
+
+                    //Single event given as string
+                    if ( value.event === event ) {
+                        handleEvent( domAt[ 0 ], key.substring( 1 ),
+                                    value.event, value.handler, eventValue );
                     }
                 }
-            });
-        });
-        
-        //remove empty objects
-        return $.grep(at, function (o) {
-            return $.isEmptyObject(o);
-        });
-        
+            } );
+        } );
+
+        //Remove empty objects
+        return $.grep( at, function( o ) {
+            return $.isEmptyObject( o );
+        } );
+
     }
-    
+
     /*
-    update all dom elements which should be managed
+    Update all dom elements which should be managed
     */
-    $.fn.dataevent = function update(event, value) {
-        
-        this.find("[data-AT]").each(function () {
-            var dom = $(this);
-            
-            executeEvent(resolveReference(getChildAttributes(dom)), event, value);
-                                  
-        });
-        
+    $.fn.dataevent = function update( event, value ) {
+
+        this.find( "[data-AT]" ).each( function() {
+            var dom = $( this );
+
+            executeEvent( resolveReference( getChildAttributes( dom ) ), event, value );
+
+        } );
+
         return this;
     };
-    
-    
-}(jQuery));
+
+}( jQuery ) );
